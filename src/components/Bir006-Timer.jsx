fcbe7089024a233764b20aade68f8dd633305c57
Lochan09 <slochansgowdas@gmail.com>
2024-10-15 19:13:13 +0530

    gdfshdfh

--- src/components/Bir006-Timer.jsx
+++ src/components/Bir006-Timer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Bir006-Timer.css';
-import time from '../assets/Bir006-Timer/time.svg';
 import cloud1 from '../assets/Bir006-Timer/cloud1.svg';
 import cloud2 from '../assets/Bir006-Timer/cloud2.svg';
-import pattern from '../assets/Bir006-Timer/pattern.svg';
+import pattern from '../assets/Bir006-Timer/pattern.png';
+import pattern2 from '../assets/Bir006-Timer/pattern2.png';
 import info from '../assets/Bir006-Timer/info.svg';
-
+import clebrating from '../assets/Bir006-Timer/celebrating.svg';
+import banner from '../assets/Bir006-Timer/Namebanner.svg';
 
 const Bir006Timer = () => {
   return (
@@ -38,7 +39,29 @@ const Bir006Timer = () => {
             </div>
           </div>
         </div>
-        <div className='bir006-'></div>
+      <div className="Bir006-Pattern-container">
+      <div className="Bir006-imagecontainer">
+          <div className="Bir006-image-wrapper">
+            <div className="Bir006-card">
+              <div className="Bir006-front">
+                <img className="Bir006-front-image" src={pattern} alt="" />
+              </div>
+              <div className="Bir006-back">
+                <img className="Bir006-back-image" src={pattern2} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='Bir006-tdetails'>
+          <img src={clebrating} alt="" />
+          <div className='Bir006-nameand'>
+          <p>Anna's</p>
+          <p>3rd Birthday</p>
+          </div>
+          <img src={banner} alt="" />
+        </div>
+      </div>
+        
       </div>
     </section>
     </>
