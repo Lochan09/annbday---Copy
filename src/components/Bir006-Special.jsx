3aa60d26324bbdb5a2900933aef2c3e03af3c547
Ashath19 <97071508+Ashath19@users.noreply.github.com>
2024-10-15 21:07:53 +0530

    PROGReSSSS!!!!!

--- src/components/Bir006-Special.jsx
+++ src/components/Bir006-Special.jsx
@@ -2,48 +2,40 @@ import React from 'react';
 import "./Bir006-Special.css";
 import baby from '../assets/Bir006-Special/baby.svg';
 import cartoon from '../assets/Bir006-Special/cartoon.svg';
-import larr from '../assets/Bir006-Special/larr.svg';
-import rarr from '../assets/Bir006-Special/rarr.svg';
-import date from '../assets/Bir006-Special/date.svg';
 
+const imageArray = [
+  { date: '2023-10-01', image: baby },
+  { date: '2023-10-02', image: baby },
+  { date: '2023-10-03', image: baby },
+  { date: '2023-10-04', image: baby },
+  { date: '2023-10-05', image: baby },
+  { date: '2023-10-06', image: baby },
+];
 
 
 const Bir006Special = () => {
+  const imagesToShow = imageArray.slice(0, 4);
   return (
-    <div>
-      <div>
-        <div className="nimbus-container">
+    <>
+    <section id='Bir006-special'>
+      <div className='nimbus-container'>
+        <div className='bir006-specialcontainer' >
+      <h1>Special Highlights</h1>
+        <div className='Bir006-Imageset'>
           
-          <div className="Bir006-specialcontainer">
-            <div className="Bir006-title">
-              Special Highlights
+        {imagesToShow.map((item, index) => (
+            <div key={index} className='image-item'>
+              <img src={item.image} alt={`Special ${index}`} />
+              <p>{item.date}</p>
             </div>
-            <div className="Bir006-carousel">
-              <div className="Bir006-larrow">
-                <i className="Bir006-larrow"></i>
-                <img alt="left arrow" height="100" src={larr} width="100" />
-              </div>
-              {[1, 2, 3, 4].map((index) => (
-                <div key={index} className="Bir006-card">
-                  <img alt="Baby lying on a blanket with a pacifier" height="300" src={baby} width="200" />
-                  <div className="Bir006-date">
-                  <img alt="date" height="300" src={date} width="200" />
-                  </div>
-                  
-                </div>
-              ))}
-              <div className="Bir006-rarrow">
-                <i className="Bir006-fas fa-arrow-right"></i>
-                <img alt="right arrow" height="100" src={rarr} width="100" />
-              </div>
-            </div>
-            <div className="Bir006-footer">
-              <img alt="Cartoon children holding hands" height="500" src={cartoon} width="600" />
-            </div>
-          </div>
+          ))}
         </div>
+        
+        <img src={cartoon} alt="" />
       </div>
-    </div>
+      </div>
+    </section>
+    </>
   );
 };
 
